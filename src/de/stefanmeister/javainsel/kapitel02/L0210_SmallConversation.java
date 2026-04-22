b019c4f596b87729f04ac344de469f47b2979454
stefa <stefa@DESKTOP-8RS1NAF.fritz.box>
2026-04-22 23:48:00 +0200

    Bis 2.10

---
+++ src/de/stefanmeister/javainsel/kapitel02/L0210_SmallConversation.java
@@ -0,0 +1,18 @@
+package de.stefanmeister.javainsel.kapitel02;
+
+public class L0210_SmallConversation 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Moin! Wie heißt denn du?");
+		String name = new java.util.Scanner(System.in).nextLine();
+		System.out.printf("Hallo %s. Wie alt bist du?%n", name);
+		int age = new java.util.Scanner(System.in).nextInt();
+		System.out.printf("Aha, %s Jahre, das ist ja die Hälfte von %s.%n", age, age * 2);
+		System.out.println("Sag mal, was ist deineLieblingsfließkommazahl?");
+		double value = new java.util.Scanner(System.in).nextDouble();
+		System.out.printf("%s? Aha, meine ist %s.%n",  value, Math.random() * 100000);
+	}
+
+}
