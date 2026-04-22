b59d85e8a9673304306d80cd9ede1932a3bc2d67
stefa <stefa@DESKTOP-8RS1NAF.fritz.box>
2026-04-22 20:20:16 +0200

    Initial load

---
+++ src/javainsel/kapitel01/Squares.java
@@ -0,0 +1,27 @@
+package javainsel.kapitel01;
+
+// Listing 1.1, Seite 83
+
+public class Squares 
+{
+	static int quadrat(int n)
+	{
+		return n * n;
+	}
+
+	static void ausgabe(int n)
+	{
+		for(int i = 1; i <= n; i = i + 1) 
+		{
+			String s = "Quadrat(" + i + ") = " + quadrat(i);
+			System.out.println(s);
+		}
+	}
+
+
+	public static void main(String[] args) 
+	{
+		ausgabe(30);
+	}
+
+}
